d1f01dfd970db0a4623973cc4b7924460b679bb3
Ben Groves <benjamin.groves@bskyb.com>
2014-10-28 12:39:12 +0000

    no message

--- recipes/lamp_bootstrap.sh
+++ recipes/lamp_bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #Apache, PHP and MySQL install script
-yum -y install httpd php mysql php-mysql mysql-server
+yum -y install httpd php mysql php-mysql mysql-server git
 chkconfig httpd on
 /etc/init.d/httpd start
 chkconfig mysqld on
 service mysqld start
+sudo chown -R ec2-user /var/www/html
+sudo chmod -R 777 /var/www/html
