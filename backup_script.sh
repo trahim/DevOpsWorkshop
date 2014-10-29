ed00094f80b4b49244b461a5f9369d99c17c3062
Eugene Tolan <Eugene.Tolan@bskyb.com>
2014-10-29 15:39:41 +0000

    Correct animal.sql path

--- backup_script.sh
+++ backup_script.sh
@@ -2,4 +2,4 @@
 #backup script 
 sudo yum update 
 sudo aws s3 cp /var/www/html/wordpress/wordpress_db.sql s3://webtcrotch/wordpress_db.sql
-sudo aws s3 cp /var/www/html/angus/animal.sql s3://webtcrotch/wordpress_db.sql
+sudo aws s3 cp /var/www/html/angus/animal.sql s3://webtcrotch/animal.sql
