c0d4337849ee9c625ba0fe374c54e9556f29452e
Rahim <tra06@ostml0222615.bskyb.com>
2014-10-29 11:53:18 +0000

    Added Maven config

--- recipes/lamp_bootstrap.sh
+++ recipes/lamp_bootstrap.sh
@@ -26,6 +26,19 @@ cp -R DevOpsWorkshop/wordpress /var/www/html
 cd DevOpsWorkshop/wordpress
 mysql < wordpress_db.sql
 #Tom cat installation
-yum -y install tomcat6 tomcat6-webapps tomcat6-admin-webapps
-service tomcat6 start
-
+yum -y install tomcat7 tomcat7-webapps tomcat7-admin-webapps
+service tomcat7 start
+#Download maven
+mkdir /usr/local/apache-maven
+cd /usr/local/apache-maven
+wget http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+tar zxvf apache-maven-3.2.3-bin.tar.gz
+M2_HOME=/usr/local/apache-maven/apache-maven-3.2.3/
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+#Download java sdk
+yum install java-1.7.0-openjdk-devel
+cd ~
+git clone https://bitbucket.org/kizzie/webservices.git
+cd webservices/SimpleWebApplication
+mvn package
